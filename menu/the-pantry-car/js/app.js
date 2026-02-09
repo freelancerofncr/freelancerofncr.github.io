02f0e78b9e8c4bb12e2a83c192b7ecb089f40ec7
Freelancer of NCR <256092415+freelancerofncr@users.noreply.github.com>
2026-02-09 12:25:59 +0530

    Update app.js

--- menu/the-pantry-car/js/app.js
+++ menu/the-pantry-car/js/app.js
@@ -29,7 +29,7 @@ function renderBusiness(data) {
   setText("#categoryLine", data.identity.categoryLine);
 
   if (data.identity.hasLogo) {
-    setImage("#restaurantLogo", "./assets/logo.png");
+    setImage("#restaurantLogo", "./assets/logo.jpeg");
   }
 
   /* ===== VEG / NON-VEG BADGE ===== */
