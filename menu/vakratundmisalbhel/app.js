12a86598fd29419de10fdc58b1ffaee50727f067
Freelancer of NCR <256092415+freelancerofncr@users.noreply.github.com>
2026-02-11 13:24:28 +0530

    Update app.js

--- menu/vakratundmisalbhel/app.js
+++ menu/vakratundmisalbhel/app.js
@@ -29,7 +29,7 @@ function renderBusiness(data) {
   setText("#categoryLine", data.identity.categoryLine);
 
   if (data.identity.hasLogo) {
-    setImage("#restaurantLogo", "./assets/logo.png");
+    setImage("#restaurantLogo", "./assets/logo.jpeg");
   }
 
   /* ===== VEG / NON-VEG BADGE ===== */
@@ -99,7 +99,7 @@ function renderBusiness(data) {
 
   /* ===== PAYMENT ===== */
   if (data.payment.enabled) {
-    setImage("#paymentQR", "./assets/payment.png");
+    setImage("#paymentQR", "./assets/payment.jpeg");
   }
 
   /* ===== ONLINE PLATFORMS (NO CHANGE) ===== */
