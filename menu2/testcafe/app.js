6d04b69d8413625a4f581420aa9c5e88c03c5a01
Freelancer of NCR <256092415+freelancerofncr@users.noreply.github.com>
2026-03-01 21:03:13 +0530

    Update app.js

--- menu2/testcafe/app.js
+++ menu2/testcafe/app.js
@@ -1093,7 +1093,7 @@ cart.forEach((item, index) => {
 
 const totalMsg = cart.reduce((s, i) => s + i.qty * i.price, 0);
 
-message += `*--------------------------------------*\n\n`;
+message += `*--------------------------------------*\n`;
 message += `*Total Amount:* ₹${totalMsg}\n`;
 message += `*--------------------------------------*\n`;
 message += `*Customer Name:* ${name}\n`;
